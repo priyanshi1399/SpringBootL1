32197c5ca8e28821586a9dc43abb0b33e3dc9e3b
priyanshi1399 <priyanshitomar001@gmail.com>
2025-06-23 22:46:21 +0530

    created project

---
+++ src/main/java/com/priyanshi/jdbl_L1/L1SpringBoot/L1SpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.priyanshi.jdbl_L1.L1SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1SpringBootApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(L1SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshi/jdbl_L1/L1SpringBoot/L1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshi.jdbl_L1.L1SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
